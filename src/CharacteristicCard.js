0fc47bae5cc48bec1673d1a5f10474281559cc18
botamochi6277 <botamochi6277@icloud.com>
2022-07-05 19:01:21 +0900

    update properties chip

--- src/CharacteristicCard.js
+++ src/CharacteristicCard.js
@@ -16,6 +16,12 @@ import CardActions from '@mui/material/CardActions';
 
 import ErrorIcon from '@mui/icons-material/Error';
 
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import EditIcon from '@mui/icons-material/Edit';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import PodcastsIcon from '@mui/icons-material/Podcasts';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
 
 const utf8_decoder = new TextDecoder('utf-8')
 // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/DataView
@@ -60,20 +66,72 @@ function BLETypeSelect(props) {
 }
 
 function PropertiesChip(props) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/BluetoothCharacteristicProperties
     const properties = props.properties;
-    /** @type {string}  */
-    let s = "";
-    if (properties.read) {
-        s += "Read|"
+    let chip_state = [];
+
+    if (properties.authenticatedSignedWrites) {
+        chip_state.push(
+            { name: "authenticatedSignedWrites", icon: <EditOutlinedIcon /> }
+        )
     }
-    if (properties.write) {
-        s += "Write|"
+
+    if (properties.broadcast) {
+        chip_state.push(
+            { name: "broadcast", icon: <PodcastsIcon /> }
+        )
     }
+
+    if (properties.indicate) {
+        chip_state.push(
+            { name: "indicate", icon: <LightbulbIcon /> }
+        )
+    }
+
     if (properties.notify) {
-        s += "Notify";
+        chip_state.push(
+            { name: "Notify", icon: <NotificationsIcon /> }
+        )
     }
+
+    if (properties.read) {
+        chip_state.push(
+            { name: "Read", icon: <MenuBookIcon /> }
+        );
+    }
+
+    if (properties.reliableWrite) {
+        chip_state.push(
+            { name: "reliableWrite", icon: <EditOutlinedIcon /> }
+        );
+    }
+
+    if (properties.writableAuxiliaries) {
+        chip_state.push(
+            { name: "writableAuxiliaries", icon: <EditOutlinedIcon /> }
+        );
+    }
+
+    if (properties.write) {
+        chip_state.push(
+            { name: "write", icon: <EditIcon /> }
+        );
+    }
+
+    if (properties.writeWithoutResponse) {
+        chip_state.push(
+            { name: "writeWithoutResponse", icon: <EditIcon /> }
+        );
+    }
+
+
+    const chips = chip_state.map(
+        (c) =>
+            <Chip label={c.name} icon={c.icon} key={c.name}></Chip>
+    )
+
     return (
-        <Chip label={s} />
+        chips
     )
 }
 

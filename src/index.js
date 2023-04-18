a61a7a2b80a397d8ed2b7825f8c402c34169396c
botamochi6277 <botamochi6277@icloud.com>
2023-04-18 20:42:39 +0900

    add helmet

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <title>BLE Console</title>
+      </Helmet>
+    </HelmetProvider>
     <App />
   </React.StrictMode>
 );

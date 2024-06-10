3c42b2682613c90f0b981cfe57842b1f9104516d
botamochi6277 <botamochi6277@icloud.com>
2024-06-10 21:29:45 +0900

    Change basepath

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://qiita.com/tat_mae084/items/4051c61926dc8165e80b
 export default defineConfig({
   base: process.env.GITHUB_PAGES
-    ? "reactjs-ble-console"
+    ? "reactjs-ble-console/"
     : "./",
   plugins: [react(),
   VitePWA({
